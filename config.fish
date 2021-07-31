d3eeb0e498a19abf3728bd20d3fe6a5e5d7b7f43
Thomas Castleman <thomascastleman0@gmail.com>
2021-07-31 09:34:57 -0400

    add files

---
+++ config.fish
@@ -0,0 +1,51 @@
+
+alias g="git"
+alias c="cargo"
+alias cl="clear"
+alias start-tmux="TERM=tmux tmux -2"
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
+
+function fish_prompt
+	set_color --bold brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color --bold blue
+	echo -n (hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color --bold brcyan
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color 60f0c5
+	echo -n '$ '
+	set_color normal
+end
+
+function fish_greeting
+	echo
+	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e " \\e[1mDisk usage:\\e[0m"
+	echo
+	echo -ne (\
+		df -l -h | grep -E 'dev/(xvda|sd|mapper)' | \
+		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
+		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
+		paste -sd ''\
+	)
+	echo
+end
+
+set PATH $HOME/.cargo/bin $PATH
+
+# opam configuration
+source /home/tc/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
